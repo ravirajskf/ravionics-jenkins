e32058fa97d971f4f8f29f3dc99db3e314535f10
Ravionics <ravirajfromindia@gmail.com>
2023-09-12 16:55:24 +0530

    first commit

---
+++ src/main/java/com/ravionics/ravionicsjenkins/RavionicsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.ravionics.ravionicsjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavionicsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavionicsJenkinsApplication.class, args);
+	}
+
+}
